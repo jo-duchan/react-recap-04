4a37098908327aa24d73beb69d407d0044572cb0
jo-duchan <jo_duchan@icloud.com>
2023-05-22 15:08:08 +0900

    section11-154

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,12 +1,14 @@
+import React, { forwardRef } from "react";
+
 import classes from "./Input.module.css";
 
-function Input(props) {
+function Input(props, ref) {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input id={props.input.id} {...props.input} />
+      <input ref={ref} id={props.input.id} {...props.input} />
     </div>
   );
 }
 
-export default Input;
+export default forwardRef(Input);
