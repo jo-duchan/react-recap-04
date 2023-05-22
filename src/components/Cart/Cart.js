4b897ffa8fce319dc76d6d72ca056c3bf12fe150
jo-duchan <jo_duchan@icloud.com>
2023-05-22 17:31:29 +0900

    section11-155

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 
+import CartContext from "store/cart-context";
 import Modal from "components/UI/Modal";
+import CartItem from "./CartItem";
 import calsses from "./Cart.module.css";
 
 function Cart(props) {
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={calsses["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -16,11 +32,13 @@ function Cart(props) {
       {cartItems}
       <div className={calsses.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={calsses.actions}>
-        <button className={calsses["button--alt"]} onClick={props.onClose}>Close</button>
-        <button className={calsses.button}>Order</button>
+        <button className={calsses["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={calsses.button}>Order</button>}
       </div>
     </Modal>
   );
