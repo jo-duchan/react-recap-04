1bfc36103f23076dfd3ea84f0d4a8d1901c35f77
jo-duchan <jo_duchan@icloud.com>
2023-06-27 23:13:20 +0900

    Section17-228

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,8 +7,11 @@ import classes from "./AvailableMeals.module.css";
 
 function AvailableMeals() {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     const fetchMeals = async () => {
+      setIsLoading(true);
       const response = await fetch(
         "https://react-http-ab44d-default-rtdb.firebaseio.com/meals.json"
       );
@@ -25,10 +28,19 @@ function AvailableMeals() {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
