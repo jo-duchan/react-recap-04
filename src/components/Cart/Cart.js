8adbcfe153ca74a07b604020ad5df307c2cb76c8
jo-duchan <jo_duchan@icloud.com>
2023-07-04 23:34:30 +0900

    Section17-종료 별도 에러케이스 작업

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,6 +10,7 @@ function Cart(props) {
   const [isCheckout, setIsCheckout] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [didSubmit, setDidSubmit] = useState(false);
+  const [error, setError] = useState(null);
   const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
@@ -27,16 +28,24 @@ function Cart(props) {
 
   const submitOrderHandler = async (userData) => {
     setIsSubmitting(true);
-    await fetch(
-      "https://react-http-ab44d-default-rtdb.firebaseio.com/orders.json",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          user: userData,
-          orderedItems: cartCtx.items,
-        }),
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://react-http-ab44d-default-rtdb.firebaseio.com/orders.json",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            user: userData,
+            orderedItems: cartCtx.items,
+          }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
       }
-    );
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
     setIsSubmitting(false);
     setDidSubmit(true);
     cartCtx.clearCart();
@@ -97,11 +106,23 @@ function Cart(props) {
     </Fragment>
   );
 
+  const errorModalContent = (
+    <Fragment>
+      <p className={calsses["cart-error"]}>{error}</p>
+      <div className={calsses.actions}>
+        <button className={calsses.button} onClick={props.onClose}>
+          Close
+        </button>
+      </div>
+    </Fragment>
+  );
+
   return (
     <Modal onClose={props.onClose}>
-      {!isSubmitting && !didSubmit && cartModalContent}
+      {error && errorModalContent}
+      {!isSubmitting && !didSubmit && !error && cartModalContent}
       {isSubmitting && isSubmittingModalContent}
-      {!isSubmitting && didSubmit && didSubmitModalContent}
+      {!isSubmitting && didSubmit && !error && didSubmitModalContent}
     </Modal>
   );
 }
